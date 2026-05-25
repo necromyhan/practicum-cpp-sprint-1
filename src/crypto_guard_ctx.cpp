ffb0b8b1254b78a96d7f0a50a9dcee631af8a93f
Tyufyagin Mikhail <mixtumi@yandex.ru>
2026-05-25 09:52:47 +0000

    Объявление класса PImpl

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,64 @@
+#include "crypto_guard_ctx.h"
+#include <array>
+#include <iostream>
+#include <openssl/err.h>
+#include <openssl/evp.h>
 
 namespace CryptoGuard {
+struct AesCipherParams {
+    static const size_t KEY_SIZE = 32;             // AES-256 key size
+    static const size_t IV_SIZE = 16;              // AES block size (IV length)
+    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
+
+    int encrypt;                              // 1 for encryption, 0 for decryption
+    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
+    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
+};
+
+class CryptoGuardCtx::PImpl {
+public:
+    PImpl() { OpenSSL_add_all_algorithms(); }
+
+    ~PImpl() { EVP_cleanup(); }
+
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+
+private:
+    AesCipherParams CreateCipherParamsFromPassword(std::string_view password) {
+        AesCipherParams params;
+        constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+        int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
+                                    reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
+                                    params.key.data(), params.iv.data());
+
+        if (result == 0) {
+            throw std::runtime_error{"Failed to create a key from password"};
+        }
+
+        return params;
+    }
+};
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<PImpl>()) {}
+
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+CryptoGuardCtx::CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
+CryptoGuardCtx &CryptoGuardCtx::operator=(CryptoGuardCtx &&) noexcept = default;
+
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->EncryptFile(inStream, outStream, password);
+}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->DecryptFile(inStream, outStream, password);
+}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) { return pImpl_->CalculateChecksum(inStream); }
 
 }  // namespace CryptoGuard
